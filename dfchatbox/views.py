dc35ff6e6b94fe4e51e525fcb4390f883a513b2d
random <random>
2018-09-25 12:08:56 +0200

    Debug

--- dfchatbox/views.py
+++ dfchatbox/views.py
@@ -890,6 +890,7 @@ def searchForEntry(answer_json):
 
 	#Use provided ehrid
 	parameter_ehrid = answer_json['result']['parameters']['ehrid']
+	context = [context_n for context_n in answer_json["result"]["contexts"] if context_n["name"] == "user_data"]
 
 	if parameter_name == "" and parameter_last_name == "" and parameter_ehrid == "" and context["parameters"]["user_isDoctor"] != "true":
 		parameter_ehrid = context["parameters"]["user_ehrid"]
