e911080f6a1b73a80cc23eae6d10d8b5ae512b11
random <random>
2018-11-30 13:34:21 +0100

    debug

--- dfchatbox/views.py
+++ dfchatbox/views.py
@@ -168,7 +168,7 @@ def check_links(request):
 
 @require_http_methods(['GET'])
 def entry_tree(request):
-	print("request @ entry_tree\n",request,"\n")
+	print("request @ entry_tree\n",request.text,"\n")
 	dataLength = cache.get("dataLength")
 	cache.delete("dataLength")
 	print("\n=== DATA LENGTH @ ENTRY_TREE: ===>  ", dataLength,"\n")
